306b2366a2dbc020a27993b695151088c845d007
Fraser <fraser@berlinguettegroup.com>
2021-07-10 19:36:12 -0700

    Small change.

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ def run():
     wiringpi.wiringPiSetupGpio()
     serial = wiringpi.serialOpen('/dev/ttyS0', 9600)
     wiringpi.serialPuts(serial, 'P2')
-    wiringpi.serialPuts(serial, 'OP:111')
+    wiringpi.serialPuts(serial, 'OP:1111')
 
 
 if __name__ == '__main__':
