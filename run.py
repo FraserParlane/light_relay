7d0da55ac47efac94d506a7980e33c574acc49fa
Fraser <fraser@berlinguettegroup.com>
2021-07-10 17:16:27 -0700

    2 Hz test

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 import wiringpi
+import time
 
 def run():
     wiringpi.wiringPiSetup()
     serial = wiringpi.serialOpen('/dev/ttyAMA0', 9600)
-    print(serial)
+    while True:
+        print(serial)
+        time.sleep(0.5)
+        wiringpi.serialPuts(serial, 'on')
+        time.sleep(0.5)
+        wiringpi.serialPuts(serial, 'off')
 
 if __name__ == '__main__':
     run()
