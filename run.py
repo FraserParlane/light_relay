bcb4961564160bf0f8a24d5b35af87077b515ca5
Fraser <fraser@berlinguettegroup.com>
2021-07-10 19:39:31 -0700

    Small change.

--- run.py
+++ run.py
@@ -1,11 +1,17 @@
 import wiringpi
 import time
 
+
+def send_command(serial, command):
+    wiringpi.serialPuts(serial, command)
+    time.sleep(0.5)
+
+
 def run():
     wiringpi.wiringPiSetupGpio()
     serial = wiringpi.serialOpen('/dev/ttyS0', 9600)
-    wiringpi.serialPuts(serial, 'P3')
-    wiringpi.serialPuts(serial, 'OP1131')
+    for i in range(9):
+        send_command(serial, f'OP:000{i}')
 
 
 if __name__ == '__main__':
