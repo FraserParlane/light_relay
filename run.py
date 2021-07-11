c2c1da8618c0b1c3b5dce04d9a4bc74e0df17d57
Fraser <fraser@berlinguettegroup.com>
2021-07-10 19:34:52 -0700

    Small change.

--- run.py
+++ run.py
@@ -5,15 +5,8 @@ def run():
     wiringpi.wiringPiSetupGpio()
     serial = wiringpi.serialOpen('/dev/ttyS0', 9600)
     print(serial)
-    wiringpi.serialPuts(serial, 'P7')
-    timer = 5
-    for i in range(7):
-        time.sleep(timer)
-        print('I')
-        wiringpi.serialPuts(serial, 'on')
-        # time.sleep(timer)
-        # print('O')
-        # wiringpi.serialPuts(serial, 'off')
+    wiringpi.serialPuts(serial, 'P1')
+    wiringpi.serialPuts(serial, 'OP: 1111')
 
 
 if __name__ == '__main__':
